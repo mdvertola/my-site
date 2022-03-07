f476a064f181f668afa07eb7e6a009f48789f766
mdvertola <mdvertola@gmail.com>
2022-03-06 18:09:08 -0700

    added base angular site

---
+++ site/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { ExpComponent } from './components/exp/exp.component';
+import { MainComponent } from './routes/main/main.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutMeComponent,
+    ExpComponent,
+    MainComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
